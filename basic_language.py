862407401535323a5e74c15b4f835be834a6f9ae
Youngermaster <jmyoungh@eafit.edu.co>
2019-05-06 14:23:10 -0500

    I finished all the language

--- basic_language.py
+++ basic_language.py
@@ -1,7 +1,6 @@
 from sly import Lexer
 from sly import Parser
 
-
 class BasicLexer(Lexer):
     tokens = { NAME, NUMBER, STRING, IF, THEN, ELSE, FOR, FUN, TO, ARROW, EQEQ }
     ignore = '\t '
@@ -22,165 +21,189 @@ class BasicLexer(Lexer):
     EQEQ = r'=='
 
     @_(r'\d+')
-    def NUMBER(self, token):
-        token.value = int(token.value)
-        return token
+    def NUMBER(self, t):
+        t.value = int(t.value)
+        return t
 
     @_(r'#.*')
-    def COMMENT(self, token):
+    def COMMENT(self, t):
         pass
 
     @_(r'\n+')
-    def newline(self,token):
-        self.lineno = token.value.count('\n')
-
+    def newline(self,t ):
+        self.lineno = t.value.count('\n')
 
 class BasicParser(Parser):
     tokens = BasicLexer.tokens
+
     precedence = (
         ('left', '+', '-'),
         ('left', '*', '/'),
-        ('right', 'UNMINUS'),
-    )
+        ('right', 'UMINUS'),
+        )
 
     def __init__(self):
         self.env = { }
 
     @_('')
-    def statement(self, parser):
+    def statement(self, p):
         pass
 
     @_('FOR var_assign TO expr THEN statement')
-    def statement(self, parser):
-        return ('for_loop', ('for_loop_setup', parser.var_assign, parser.expr), parser.statement)
+    def statement(self, p):
+        return ('for_loop', ('for_loop_setup', p.var_assign, p.expr), p.statement)
 
     @_('IF condition THEN statement ELSE statement')
-    def statement(self, parser):
-        return ('if_stmt', parser.condition, ('branch', p.statement0, p.statement1))
+    def statement(self, p):
+        return ('if_stmt', p.condition, ('branch', p.statement0, p.statement1))
 
     @_('FUN NAME "(" ")" ARROW statement')
-    def statement(self, parser):
-        return ('fun_def', parser.NAME, parser.statement)
+    def statement(self, p):
+        return ('fun_def', p.NAME, p.statement)
 
     @_('NAME "(" ")"')
-    def statement(self, parser):
-        return ('fun_call', parser.NAME)
+    def statement(self, p):
+        return ('fun_call', p.NAME)
 
     @_('expr EQEQ expr')
-    def condition(self, parser):
-        return ('condition_eqeq', parser.expr0, parser.expr1)
+    def condition(self, p):
+        return ('condition_eqeq', p.expr0, p.expr1)
 
     @_('var_assign')
-    def statement(self, parser):
-        return parser.var_assign
+    def statement(self, p):
+        return p.var_assign
 
     @_('NAME "=" expr')
-    def var_assign(self, parser):
-        return ('var_assign', parser.NAME, parser.expr)
+    def var_assign(self, p):
+        return ('var_assign', p.NAME, p.expr)
 
     @_('NAME "=" STRING')
-    def var_assign(self, parser):
-        return ('var_assign', parser.NAME, parser.STRING)
+    def var_assign(self, p):
+        return ('var_assign', p.NAME, p.STRING)
 
     @_('expr')
-    def statement(self, parser):
-        return (parser.expr)
+    def statement(self, p):
+        return (p.expr)
 
     @_('expr "+" expr')
-    def expr(self, parser):
-        return ('add', parser.expr0, parser.expr1)
+    def expr(self, p):
+        return ('add', p.expr0, p.expr1)
 
     @_('expr "-" expr')
-    def expr(self, parser):
-        return ('sub', parser.expr0, parser.expr1)
+    def expr(self, p):
+        return ('sub', p.expr0, p.expr1)
 
     @_('expr "*" expr')
-    def expr(self, parser):
-        return ('mul', parser.expr0, parser.expr1)
+    def expr(self, p):
+        return ('mul', p.expr0, p.expr1)
 
     @_('expr "/" expr')
-    def expr(self, parser):
-        return ('div', parser.expr0, parser.expr1)
+    def expr(self, p):
+        return ('div', p.expr0, p.expr1)
 
-    @_('"-" expr %prec UNMINUS')
-    def expr(self, parser):
-        return parser.expr
+    @_('"-" expr %prec UMINUS')
+    def expr(self, p):
+        return p.expr
 
     @_('NAME')
-    def expr(self, parser):
-        return ('var', parser.NAME)
+    def expr(self, p):
+        return ('var', p.NAME)
 
     @_('NUMBER')
-    def expr(self, parser):
-        return ('num', parser.NUMBER)
+    def expr(self, p):
+        return ('num', p.NUMBER)
 
-    
+
+
 class BasicExecute:
+
     def __init__(self, tree, env):
         self.env = env
         result = self.walkTree(tree)
-        if result is not None and isInstance(result, int):
+        if result is not None and isinstance(result, int):
             print(result)
-        if isInstance(result, str) and result[0] == '"':
+        if isinstance(result, str) and result[0] == '"':
             print(result)
 
     def walkTree(self, node):
-        if isInstance(node, int):
+
+        if isinstance(node, int):
             return node
-        if isInstance(node, int):
+        if isinstance(node, str):
             return node
+
         if node is None:
             return None
+
         if node[0] == 'program':
             if node[1] == None:
                 self.walkTree(node[2])
             else:
                 self.walkTree(node[1])
                 self.walkTree(node[2])
+
         if node[0] == 'num':
             return node[1]
+
         if node[0] == 'str':
             return node[1]
+
         if node[0] == 'if_stmt':
             result = self.walkTree(node[1])
             if result:
                 return self.walkTree(node[2][1])
             return self.walkTree(node[2][2])
+
         if node[0] == 'condition_eqeq':
             return self.walkTree(node[1]) == self.walkTree(node[2])
+
         if node[0] == 'fun_def':
             self.env[node[1]] = node[2]
+
         if node[0] == 'fun_call':
             try:
                 return self.walkTree(self.env[node[1]])
             except LookupError:
                 print("Undefined function '%s'" % node[1])
                 return 0
+
         if node[0] == 'add':
             return self.walkTree(node[1]) + self.walkTree(node[2])
-        if node[0] == 'sub':
+        elif node[0] == 'sub':
             return self.walkTree(node[1]) - self.walkTree(node[2])
-        if node[0] == 'mul':
+        elif node[0] == 'mul':
             return self.walkTree(node[1]) * self.walkTree(node[2])
-        if node[0] == 'div':
+        elif node[0] == 'div':
             return self.walkTree(node[1]) / self.walkTree(node[2])
+
         if node[0] == 'var_assign':
-            self.env[node[1] = self.walkTree(node[2])]
+            self.env[node[1]] = self.walkTree(node[2])
             return node[1]
+
         if node[0] == 'var':
             try:
                 return self.env[node[1]]
-            except LookupError as identifier:
-                print("Undefined variable '" + node[1] + "' found!")
+            except LookupError:
+                print("Undefined variable '"+node[1]+"' found!")
                 return 0
+
         if node[0] == 'for_loop':
             if node[1][0] == 'for_loop_setup':
                 loop_setup = self.walkTree(node[1])
+
                 loop_count = self.env[loop_setup[0]]
-                loop_limit = looṕ_setup[1]
+                loop_limit = loop_setup[1]
+
+                for i in range(loop_count+1, loop_limit+1):
+                    res = self.walkTree(node[2])
+                    if res is not None:
+                        print(res)
+                    self.env[loop_setup[0]] = i
+                del self.env[loop_setup[0]]
 
         if node[0] == 'for_loop_setup':
             return (self.walkTree(node[1]), self.walkTree(node[2]))
+
 
 if __name__ == '__main__':
     lexer = BasicLexer()
@@ -199,6 +222,4 @@ if __name__ == '__main__':
                 print(token)
             """
             tree = parser.parse(lexer.tokenize(text))
-            # print(tree)
             BasicExecute(tree, env)
-            # print(env)
